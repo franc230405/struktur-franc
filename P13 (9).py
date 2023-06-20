73a8239553a027bdb117682b8b7fe2bd5799248f
franc230405 <136425519+franc230405@users.noreply.github.com>
2023-06-20 10:13:40 +0700

    Add files via upload

---
+++ P13 (9).py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#   Created by Elshad Karimov 
+#   Copyright © AppMillers. All rights reserved.
+
+# Searching algorithms - Linear Search
+
+def linearSearch(array, value):
+    for i in range(len(array)):
+        if array[i] == value:
+            return i
+    return -1
+
+
+
+print(linearSearch([20,40,30,50,90], 90))
+
